ae6c5a07c67f39c904fffdf5765b3b45386164ef
hamdy <hamdyfathy010@gmail.com>
2024-03-07 18:45:26 +0200

    app

--- lib/widget/list_item.dart
+++ lib/widget/list_item.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 import 'package:shopping_app/colors/color_app.dart';
 import 'package:shopping_app/models/cart_model.dart';
@@ -15,59 +17,92 @@ class ListItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        const SizedBox(
-          width: 10,
-        ),
-        Container(
-          width: 100,
-          height: 100,
-          decoration: BoxDecoration(
-              image: DecorationImage(image: AssetImage(image)),
-              borderRadius: BorderRadius.circular(20)),
-        ),
-        const SizedBox(
-          width: 20,
-        ),
-        Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Text(
-              name,
-              style: const TextStyle(
-                  color: Color(0xff999999), fontFamily: 'NunitoSans'),
-            ),
-            const SizedBox(
-              width: 6,
-            ),
-            Text(
-              money,
-              style: const TextStyle(
-                  fontFamily: 'NunitoSans', fontWeight: FontWeight.w700),
-            ),
-            const SizedBox(
-              width: 23,
-            ),
-            Row(
-              children: [
-                Container(
-                  width: 20,
-                  decoration: BoxDecoration(
-                      color: AppColor.placeholder,
-                      borderRadius: BorderRadius.circular(20)),
-                  child: const Icon(Icons.add),
-                ),
-                const Text('01'),
-                IconButton(onPressed: () {}, icon: const Icon(Icons.remove)),
-              ],
-            ),
-          ],
-        ),
-        Expanded(
-          child: Image.asset('assets/images/Shape.png'),
-        ),
-      ],
+    return SizedBox(
+      height: 100,
+      child: Row(
+        children: [
+          const SizedBox(
+            width: 10,
+          ),
+          Container(
+            width: 100,
+            height: 100,
+            decoration: BoxDecoration(
+                image: DecorationImage(image: AssetImage(image)),
+                borderRadius: BorderRadius.circular(20)),
+          ),
+          const SizedBox(
+            width: 20,
+          ),
+          Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(
+                name,
+                style: const TextStyle(
+                    color: Color(0xff999999), fontFamily: 'NunitoSans'),
+              ),
+              const SizedBox(
+                width: 6,
+              ),
+              Text(
+                money,
+                style: const TextStyle(
+                    fontFamily: 'NunitoSans', fontWeight: FontWeight.w700),
+              ),
+              const SizedBox(
+                width: 23,
+              ),
+              Row(
+                children: [
+                  Container(
+                    alignment: Alignment.center,
+                    padding: const EdgeInsets.all(5),
+                    width: 30,
+                    height: 30,
+                    decoration: BoxDecoration(
+                        color: AppColor.stroke_divider,
+                        borderRadius: BorderRadius.circular(10)),
+                    child: const Icon(
+                      Icons.add,
+                      size: 20,
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 5,
+                  ),
+                  const Text('01'),
+                  const SizedBox(
+                    width: 5,
+                  ),
+                  Container(
+                    alignment: Alignment.center,
+                    padding: const EdgeInsets.all(5),
+                    width: 30,
+                    height: 30,
+                    decoration: BoxDecoration(
+                        color: AppColor.stroke_divider,
+                        borderRadius: BorderRadius.circular(10)),
+                    child: const Icon(
+                      Icons.remove,
+                      size: 20,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+          Expanded(
+            child: Align(
+                alignment: Alignment.topRight,
+                child: Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+                  child: Image.asset('assets/images/Shape.png'),
+                )),
+          ),
+        ],
+      ),
     );
   }
 }
